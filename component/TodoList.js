7113e44b6194d6be02501b794b988c051083a60c
TNuan <minibike090@gmail.com>
2022-03-19 09:16:11 +0700

    add event & toggle checked

--- component/TodoList.js
+++ component/TodoList.js
@@ -2,14 +2,21 @@ import html from "../core.js"
 import { connect } from "../store.js"
 import TodoItem from "./TodoItem.js"
 
-function TodoList({ todos }) {
+function TodoList({ todos, filters }) {
+    /* console.log(filters) */
     /* console.log(todos) */
     return html`
         <section class="main">
-            <input id="toggle-all" class="toggle-all" type="checkbox">
+            <input
+                id="toggle-all" 
+                class="toggle-all" 
+                type="checkbox"
+                onchange="dispatch('toggleAll', this.checked)"
+                ${todos.every(filters.completed) && 'checked'}
+            >
             <label for="toggle-all">Mark all as complete</label>
             <ul class="todo-list">
-                ${todos.map(todo => TodoItem({ todo }))}
+                ${todos.map((todo, index) => TodoItem({ todo, index }))}
             </ul>
         </section>    
 `
